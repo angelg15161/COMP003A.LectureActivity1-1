8d01ae9641cb9b21f0c36f432eb9de5381e8831c
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-01-16 20:07:25 -0800

    Accessed on Rider

--- COMP003A.LectureActivity1/Program.cs
+++ COMP003A.LectureActivity1/Program.cs
@@ -1,13 +1,13 @@
 ﻿/* 
-    Author: Angel Gomez 
-    Course: COMP-003A
-    Faculty: Jonathan Cruz 
-    Purpose: Basic console app 
+ * Author: Angel Gomez 
+ * Course: COMP-003A
+ * Faculty: Jonathan Cruz 
+ * Purpose: Basic console app 
  */
 // namespace level 
 namespace COMP003A.LectureActivity1
 {
-    // class level
+    // Main class level
     internal class Program
     {
         //main level
